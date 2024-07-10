0f27648490ef082fdcf1f897fd6d3e85c61fb526
Emmanuel <zacmac807@gmail.com>
2024-07-10 11:41:40 +0300

    update on the Login

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-
-
-// function PropertyList() {
-//   return (
-//     <div className="property-list-container">
-//       <h2>Property List</h2>
-//       <p>List of properties will be displayed here.</p>
-//     </div>
-//   );
-// }
-
-// export default PropertyList;
-
-
-
-
-
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -25,11 +5,11 @@ function Market() {
   return (
     <div>
       <h2>Market</h2>
-      {/* Implement the logic to display all properties here */}
+      {/* where the Implementation of  the logic to display all properties ,inafaa kua */}
       <div>
         <Link to={`/property/1`}>Property 1</Link>
       </div>
-      {/* Add more property links */}
+      {/* Adding the more property links */}
     </div>
   );
 }
